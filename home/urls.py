d59e3c028bbfcc032921056bd63161e5026fe7ef
Bumbi <annika.borgesson@gmail.com>
2024-02-14 14:40:59 +0100

    Fix typos

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
+"""This will add the urls for the site"""
 from django.urls import path
 from . import views
 
-urlspattern = [
+urlpatterns = [
     path("", views.starting_page, name="starting_page"),
 ]
