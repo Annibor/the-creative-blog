4fa7ad407020e919ee400821f3ed7bf947e1d4a4
Bumbi <annika.borgesson@gmail.com>
2024-02-15 14:23:37 +0100

    Add comment form to views, to handle comments, validate them & take user back to page when doen

--- blog/forms.py
+++ blog/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 class CommentForm(forms.Form):
     author = forms.CharField(
-        max_length=70
+        max_length=70,
         widget=forms.TextInput(
-            attrs={'class': 'form-control', 'placeholder',: "Your Name"}
+            attrs={'class': 'form-control', 'placeholder': "Your Name"}
         ),
     )
     body = forms.CharFeild(
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import generic
 from .models import Post, Comment
+from .forms import CommentForm
 
 # Create your views here.
 #def blog_page(request):
@@ -30,10 +32,23 @@ def categories_posts(request, category):
 
 def blog_post_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+        
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": CommentForm(),
     }
 
     return render(request, "blog/post_detail.html", context)
