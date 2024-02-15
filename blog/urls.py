8d081dfd1b5de16436d61902d1b0d2e90d713ff4
Bumbi <annika.borgesson@gmail.com>
2024-02-15 13:23:01 +0100

    Fix views, urls to show new html files to show blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.blog_page, name="blog_page"),
-    
+    path("", views.blog_post_index, name="blog_post_index"),
+    path("post/<int:pk>/", views.blog_post_detail, name="blog_post_detail"),
+    path("category/<category>/", views.categories_posts, name="categories_posts"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,30 +3,30 @@ from django.views import generic
 from .models import Post, Comment
 
 # Create your views here.
-def blog_page(request):
-    """
-    The blog page view
-    """
-    return render(request, 'blog/blog.html')
+#def blog_page(request):
+   # """
+   # The blog page view
+   # """
+  #  return render(request, 'blog/blog.html')
 
 def blog_post_index(request):
     posts = Post.objects.all().order_by('-created_on')
     context = {
         "posts": posts,
     }
-    return render(request, "blog/post_index.html", context)
+    return render(request, "blog/blog.html", context)
 
 def categories_posts(request, category):
     """
     
     """
-    posts = Post.objects.filter(catagories__name__contains=category
+    posts = Post.objects.filter(categories__name__contains=category
     ).order_by("-created_on")
     context = {
-        "catagory": category,
+        "category": category,
         "posts": posts,
     }
-    return render(request, "blog/catagory.html", context)
+    return render(request, "blog/category.html", context)
 
 def blog_post_detail(request, pk):
     post = Post.objects.get(pk=pk)
