b17e4b2e426a5c5df48d45f380b2eaa5e5d442db
Bumbi <annika.borgesson@gmail.com>
2024-02-15 09:43:35 +0100

    Add blog post class to models

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=40)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    post_content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField("Category", related_name="posts")
