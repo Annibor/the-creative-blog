8f63b8156529d1f7f8339e93f6f80b58ae484ab0
Bumbi <annika.borgesson@gmail.com>
2024-02-16 21:35:11 +0100

    Add docstings to forms.pY

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,26 @@
+"""
+All the forms for the blog app.
+"""
 from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
+    """
+    A form for creating a comment.
+
+    This form is used to create comments on blog posts.
+    It provides fields for the author's name and the comment body.
+    The author's name is limited to a maximum of 70 characters.
+    The comment body is a textarea allowing multi-line input.
+
+    Attributes:
+        author (CharField): A field for the author's name.
+        body (CharField): A field for the actual comment.
+
+    Meta:
+        model (Comment): The model associated with this form (Comment).
+        fields (list): The list of fields to include in the form (author, body).
+    """
     author = forms.CharField(
         max_length=70,
         widget=forms.TextInput(
@@ -14,5 +33,11 @@ class CommentForm(forms.ModelForm):
         )
     )
     class Meta:
+        """
+        model (Comment): The model associated with this form (Comment).
+            This indicates which model the form is based on for validation and saving.
+        fields (list): The list of fields to include in the form (author, body).
+            This specifies which fields from the associated model should be included in the form
+        """
         model = Comment
         fields = ['author', 'body']
