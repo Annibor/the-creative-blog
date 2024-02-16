1e834ec51bb1eb17e996b82ed7c6f9f24a6e9566
Bumbi <annika.borgesson@gmail.com>
2024-02-16 21:02:40 +0100

    Add in Commentform again

--- blog/forms.py
+++ blog/forms.py
@@ -1,18 +1,18 @@
-#from .models import Comment
-#from django import forms
+from django import forms
+from .models import Comment
 
-#class CommentForm(forms.ModelForm):
-#    author = forms.CharField(
- #       max_length=70,
-  #      widget=forms.TextInput(
-   #         attrs={'class': 'form-control', 'placeholder': "Your Name"}
-   #     ),
-   # )
-   # body = forms.CharField(
-   #     widget=forms.Textarea(
-   #         attrs={'class': 'form-control', 'placeholder':'Leave a comment!'}
-   #     )
-   # )
-   # class Meta:
-   #     model = Comment
-   #     fields = ['author', 'body']
+class CommentForm(forms.ModelForm):
+    author = forms.CharField(
+        max_length=70,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': "Your Name"}
+        ),
+    )
+    body = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'class': 'form-control', 'placeholder':'Leave a comment!'}
+        )
+    )
+    class Meta:
+        model = Comment
+        fields = ['author', 'body']
